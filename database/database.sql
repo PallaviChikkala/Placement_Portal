85f8499e9ac2648894a52a832dceb68f542c64d9
Pallavi <pallavi720731@gmail.com>
2026-06-11 11:08:50 +0530

    Pushed latest changes

--- database/database.sql
+++ database/database.sql
@@ -1,206 +1,79 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
---
--- Host: localhost    Database: placement_portal
--- ------------------------------------------------------
--- Server version	8.0.44
+CREATE DATABASE IF NOT EXISTS placement_portal;
+USE placement_portal;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE students(
+	student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(50),
+    branch VARCHAR(50),
+    cgpa FLOAT,
+    backlogs INT,
+    skills TEXT,
+    selected_tier INT DEFAULT NULL
+);
 
---
--- Table structure for table `applications`
---
+ALTER TABLE students 
+ADD batch INT;
 
-DROP TABLE IF EXISTS `applications`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `applications` (
-  `application_id` int NOT NULL AUTO_INCREMENT,
-  `student_id` int DEFAULT NULL,
-  `job_id` int DEFAULT NULL,
-  `resume_path` text,
-  `status` text,
-  `applied_date` date DEFAULT NULL,
-  PRIMARY KEY (`application_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+UPDATE students
+SET selected_tier = NULL
+WHERE email = 'pallavi123@gmail.com';
 
---
--- Dumping data for table `applications`
---
+SELECT * FROM students;
+SELECT student_id, name, email FROM students;
+INSERT INTO students(student_id, name, email, password, branch, cgpa, backlogs, skills, selected_tier, batch)
+VALUES
+(1,"Pallavi", "pallavi123@gmail.com", "pallavi123", "AI", 9.2, 0, "Java, DSA, Full Stack Development", 1, 2028);
 
-LOCK TABLES `applications` WRITE;
-/*!40000 ALTER TABLE `applications` DISABLE KEYS */;
-/*!40000 ALTER TABLE `applications` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO students(student_id, name, email, password, branch, cgpa, backlogs, skills, selected_tier, batch)
+VALUES
+(2,"linda", "linda123@gmail.com", "linda123", "CSE", 9.1, 0, "C, CPP, HTML, CSS, MySQL",2, 2029);
 
---
--- Table structure for table `faculty`
---
+INSERT INTO students(student_id, name, email, password, branch, cgpa, backlogs, skills, selected_tier, batch)
+VALUES
+(3,"Hasini", "hasini123@gmail.com", "hasini123", "CSE", 9.5, 0, "HTML, CSS, JS, Python, C, CPP", 1, 2029);
 
-DROP TABLE IF EXISTS `faculty`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `faculty` (
-  `faculty_id` int NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`faculty_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE faculty(
+    faculty_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(50)
+    );
+    
+INSERT INTO faculty (faculty_id, name, email, password)
+VALUES (1, 'Dr. Shankar', 'drshankar@gmail.com', 'shankar123');
 
---
--- Dumping data for table `faculty`
---
+SELECT * FROM faculty;
 
-LOCK TABLES `faculty` WRITE;
-/*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
-INSERT INTO `faculty` VALUES (1,'Dr. Shankar','drshankar@gmail.com','shankar123');
-/*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE recruiters(
+	recruiter_id INT PRIMARY KEY,
+    company_name VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(50)
+    );
+    
+CREATE TABLE jobs(
+    job_id INT PRIMARY KEY,
+    company_name VARCHAR(50),
+    role VARCHAR(50) NOT NULL,
+    package_lpa DECIMAL(7,2),
+    tier ENUM('Tier 1','Tier 2','Tier 3'),
+    eligible_branches TEXT,
+    min_cgpa DECIMAL(3,2),
+    max_backlogs INT DEFAULT 0,
+    required_skills TEXT,
+    job_description TEXT,
+    deadline DATE
+    );
 
---
--- Table structure for table `jobs`
---
+SELECT * FROM jobs;
 
-DROP TABLE IF EXISTS `jobs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `jobs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `job_id` varchar(30) DEFAULT NULL,
-  `company_name` varchar(100) DEFAULT NULL,
-  `role` varchar(100) DEFAULT NULL,
-  `ctc` varchar(30) DEFAULT NULL,
-  `location` varchar(100) DEFAULT NULL,
-  `bond` varchar(50) DEFAULT 'None',
-  `cgpa_cutoff` decimal(4,2) DEFAULT '0.00',
-  `active_backlogs` int DEFAULT '0',
-  `backlog_history` int DEFAULT '0',
-  `branches` text,
-  `tier` varchar(20) DEFAULT 'Tier 1',
-  `description` text,
-  `req_aadhar` tinyint(1) DEFAULT '0',
-  `req_pan` tinyint(1) DEFAULT '0',
-  `req_other` varchar(200) DEFAULT NULL,
-  `pdf_path` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `job_id` (`job_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `jobs`
---
-
-LOCK TABLES `jobs` WRITE;
-/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
-INSERT INTO `jobs` VALUES (1,'1','TCS','Software Developer','7.00 LPA','Pune','None',7.00,0,0,'AI, CSE, ECE, EEE','Tier 2','Join the TCS digital developer team to work on next-generation cloud architectures.',0,0,NULL,NULL),(2,'2','Infosys','Specialist Programmer','20.00 LPA','Bangalore','None',9.50,0,0,'CSE, IT','Tier 1','High performance developer role working on core software products and algorithmic scaling.',0,0,NULL,NULL),(3,'3','Wipro','Full Stack Developer','8.00 LPA','Hyderabad','None',6.50,1,0,'AI, CSE, ECE','Tier 2','Design and implement web interfaces and microservice endpoints in our digital unit.',0,0,NULL,NULL);
-/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `notifications`
---
-
-DROP TABLE IF EXISTS `notifications`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notifications` (
-  `notif_id` int NOT NULL AUTO_INCREMENT,
-  `student_id` int DEFAULT NULL,
-  `message` text,
-  `link` text,
-  `is_read` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`notif_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `notifications`
---
-
-LOCK TABLES `notifications` WRITE;
-/*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
-INSERT INTO `notifications` VALUES (1,1,'New Job Posted: IBM is hiring for Full Stack Developer!','/eligible_companies',0,'2026-06-10 09:16:22'),(2,2,'New Job Posted: IBM is hiring for Full Stack Developer!','/eligible_companies',0,'2026-06-10 09:16:22'),(4,1,'New Job Posted: Amazon is hiring for Full Stack Developer!','/eligible_companies',0,'2026-06-10 09:18:24'),(5,2,'New Job Posted: Amazon is hiring for Full Stack Developer!','/eligible_companies',0,'2026-06-10 09:18:24');
-/*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `recruiters`
---
-
-DROP TABLE IF EXISTS `recruiters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `recruiters` (
-  `recruiter_id` int NOT NULL,
-  `company_name` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`recruiter_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `recruiters`
---
-
-LOCK TABLES `recruiters` WRITE;
-/*!40000 ALTER TABLE `recruiters` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recruiters` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `students`
---
-
-DROP TABLE IF EXISTS `students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `students` (
-  `student_id` int NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `branch` varchar(50) DEFAULT NULL,
-  `cgpa` float DEFAULT NULL,
-  `backlogs` int DEFAULT NULL,
-  `skills` text,
-  `selected_tier` int DEFAULT NULL,
-  `batch` int DEFAULT NULL,
-  `profile_photo` varchar(255) DEFAULT '/static/StudentLogin.png',
-  PRIMARY KEY (`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `students`
---
-
-LOCK TABLES `students` WRITE;
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Pallavi Chikkala','pallavi123@gmail.com','pallavi123','AI',9.2,0,'MySQL, Java, DSA, Full Stack Development, HTML, JavaScript, CSS',NULL,2028,'/static/uploads/student_1_githubProfile.jpg'),(2,'linda','linda123@gmail.com','linda123','CSE',9.1,0,'C, CSS, HTML, Java, CPP, MySQL, JavaScript',2,2029,'/static/uploads/student_2_githubProfile.jpg');
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-06-10 16:23:57
+CREATE TABLE applications(
+    application_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    job_id INT,
+    resume_path TEXT,
+    status TEXT,
+    applied_date DATE
+);
